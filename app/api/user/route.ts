debfbadd6e26bd1a02a2efabb654e458633bb7f1
Goudmu <goudmu@gmail.com>
2024-06-26 14:36:26 +0700

    api post user

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,11 +1,35 @@
 import { connectToDB } from "@/mongodb/connect/connect";
 import User from "@/mongodb/models/User";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async () => {
   try {
     await connectToDB();
     const user = await User.find();
+    return NextResponse.json({ user });
+  } catch (error: any) {
+    console.log(error);
+    throw new Error(error);
+  }
+};
+
+export const POST = async (req: NextRequest) => {
+  try {
+    await connectToDB();
+    const {
+      clerkId,
+      email,
+      username,
+      photo,
+      role = "barber",
+    } = await req.json();
+    const user = await User.create({
+      clerkId,
+      email,
+      username,
+      photo,
+      role,
+    });
     return NextResponse.json({ user });
   } catch (error: any) {
     console.log(error);
