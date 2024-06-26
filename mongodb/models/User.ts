0727ceb2d1c1bf030b6e8e30fe719a524128c880
Goudmu <goudmu@gmail.com>
2024-06-26 14:32:10 +0700

    webhook

--- mongodb/models/User.ts
+++ mongodb/models/User.ts
@@ -8,9 +8,10 @@ interface IUser extends Document {
 }
 
 const UserSchema: Schema = new Schema({
-  name: { type: String, required: true },
+  clerkId: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  username: { type: String, required: true },
+  photo: { type: String, required: true },
   role: {
     type: String,
     required: true,
